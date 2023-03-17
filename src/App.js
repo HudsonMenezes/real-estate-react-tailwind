3d23d4ec10da3fc642f649ae980c0987f60c067f
Hudson Menezes <hudson.bm23@gmail.com>
2023-03-17 18:22:56 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+//import Routes and route
+import { Routes, Route } from "react-router-dom";
+
+// import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
